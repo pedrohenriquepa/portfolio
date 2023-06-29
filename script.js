9ba01de87e0b812d2586f8510f1af6a39c73a319
pedrohenrique <115195103+pedrohenriquepa@users.noreply.github.com>
2023-06-29 16:19:45 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["Backend Developer", "Java Developer",
+        "Application Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
+var typed = new Typed(".none-text", {
+    strings: ["About me"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
+var typed = new Typed(".skills-text", {
+    strings: ["My skills"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
+
